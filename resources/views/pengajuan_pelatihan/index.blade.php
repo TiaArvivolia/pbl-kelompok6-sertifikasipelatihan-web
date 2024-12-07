8acdc0b63649c437c19c229b90544f666169c0ae
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-12-07 14:09:03 +0700

    Export Pengajuan Pelatihan

--- resources/views/pengajuan_pelatihan/index.blade.php
+++ resources/views/pengajuan_pelatihan/index.blade.php
@@ -5,6 +5,8 @@
     <div class="card-header">
         <h3 class="card-title">Pengajuan Pelatihan</h3>
         <div class="card-tools">
+            <a href="{{url('/pengajuan_pelatihan/export_excel')}}" class="btn btn-sm btn-primary mt-1"><i class="fa fa-file-excel"></i> Export (Excel)</a>
+            <a href="{{url('/pengajuan_pelatihan/export_pdf')}}" class="btn btn-sm btn-warning mt-1"><i class="fa fa-file-pdf"></i> Export (PDF)</a>
             @php
                 $user = auth()->user(); // Get the authenticated user
             @endphp
